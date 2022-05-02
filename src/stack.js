a880ab723ca3a79899d9c191628ef9bc4b452f1c
Alina <malinka775@yandex.ru>
2022-05-02 15:34:15 +0300

    add: implement Stack class

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(array = []){
+    this.elements = array
+  }
+  push( element ) {
+    this.elements.push(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    //throw new NotImplementedError('Not implemented');
+    return this.elements.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    //throw new NotImplementedError('Not implemented');
+    return this.elements[this.elements.length - 1]
   }
 }
 
