e9102daf005a4f3ad8a083de20263cbc192837c4
Alina <malinka775@yandex.ru>
2022-05-02 14:49:39 +0300

    add: solve linked lisk task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList( l, k ) {
+  //throw new NotImplementedError('Not implemented');
+ let node = l
+ let prevNode = l
+ if (!node) {
+   return null
+ }
+ if (node.value == k) {
+  node = node.next
+  prevNode.next = null
+  l = node
+} 
+ while (node) {
+  if (node.value !== k) {
+    prevNode = node
+    node = node.next
+  } else {
+    node = node.next
+    prevNode.next = node
+  }
+ }
+ return l
 }
+// function convertArrayToList(arr) {
+//   return arr.reverse().reduce((acc, cur) => {
+//     if (acc) {
+//       const node = new ListNode(cur);
+//       node.next = acc;
+//       return node;
+//     }
 
+//     return new ListNode(cur);
+//   }, null);
+// }
+//  let list = convertArrayToList([1,2,3,4,5,6])
+//  console.log(removeKFromList(list, 6))
 module.exports = {
   removeKFromList
 };
